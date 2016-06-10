305a8228679729d51ad7a3a521f54a704c846a1c
Puujee <u.puuje@gmail.com>
2016-06-10 18:07:00 -0400

    date time picker added

---
+++ core.js
@@ -0,0 +1,21 @@
+(function () {
+  'use strict';
+  var app = angular.module('ngui-core', []);
+  app.provider("$nguiConfig", function () {
+    var baseTemplateUrl = "/ngui";
+
+      return {
+        setBaseTemplateUrl: function (url) {
+          baseTemplateUrl = url;
+        },
+        $get: function () {
+          return {
+            get baseTemplateUrl() {
+              return baseTemplateUrl;
+            },
+          };
+        }
+      }
+
+  });
+})();
