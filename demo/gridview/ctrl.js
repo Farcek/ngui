c5be7692ef80c659095ab60e27e350fb3bf027cc
Farcek <farcek@gmail.com>
2016-06-23 11:41:12 +0900

    add gridview

--- demo/gridview/ctrl.js
+++ demo/gridview/ctrl.js
@@ -1,3 +1,18 @@
-function GridviewCtrl($scope) {
+function GridviewCtrl($scope, $nguiGridview, $http) {
+    $scope.$gridview = $nguiGridview({
+        
+    });
 
+    $scope.page = 1;
+
+    $scope.load = function () {
+        console.log('load')
+        $http.get('/demo/gridview/data.json')
+            .success(function (data) {
+                $scope.$items = data.items;
+                $scope.$total = data.total;
+            });
+    };
+
+    $scope.load();
 }
--- gridview/gridview.js
+++ gridview/gridview.js
@@ -13,13 +13,23 @@
 
     nguiModule.factory('$nguiGridview', ['$nguiConfig',
         function ($nguiConfig) {
-            return function () {
-                var columns = {};
-                return {
-                    addColumn: function (column) {
-                        columns[column.name] = column;
+            return function (options) {
+                var opts = options || {};
+                var onChangeListeners = [];
+                var self = {
+                    sorting: {},
+                    filter: {},
+                    onChange: function (handle) {
+                        onChangeListeners.push(handle);
+                    },
+                    $onChange: function () {
+                        onChangeListeners.forEach(function (handle) {
+                            handle(self);
+                        });
                     }
                 };
+
+                return self;
             };
         }
     ]);
@@ -28,47 +38,147 @@
         function ($nguiConfig, $nguiGridview) {
             return {
                 restrict: 'A',
-
-                replace: true,
                 scope: {
-                    gridview: '=nguiGridview'
+                    gridview: '=nguiGridview',
+                    onChange: '&'
                 },
+                controller: function ($scope) {
+                    var $gridview = this.$gridview = $scope.$gridview = $scope.gridview || $nguiGridview();
+                    $gridview.onChange(function () {
+                        if ($scope.onChange) {
+                            $scope.onChange();
+                        }
+                    });
+                }
+            }
+
+            return {
+                restrict: 'A',
+                terminal: true,
+                replace: true,
+                // scope: {
+                //     gridview: '=nguiGridview'
+                // },
                 templateUrl: function (elem, attrs) {
                     return attrs.templateUrl || $nguiConfig.baseTemplateUrl + '/gridview.htm';
                 },
                 controller: ['$scope',
                     function ($scope) {
-                        var $gridview = $scope.$gridview = $scope.gridview || $nguiGridview();
+                        alert(5)
+
 
                         this.addColumn = function (column) {
                             $gridview.addColumn(column);
                         };
                     }
-                ]
+                ],
+                compile: function () {
+                    alert(10)
+                    return {
+                        pre: function () {
+                            alert(11)
+                            console.log('compile', arguments)
+                        },
+                        post: function () {
+                            alert(22)
+                            console.log('compile', arguments)
+                        }
+                    }
+                },
+                link: function (s, e, a, c, t) {
+                    alert(22)
+                    console.log('link', arguments)
+                }
             };
         }
     ]);
 
-    nguiModule.directive('nguiGridviewColumn', ['$nguiConfig', '$nguiGridview',
-        function ($nguiConfig, $nguiGridview) {
+    nguiModule.directive('nguiGridviewSorting', ['$nguiConfig',
+        function ($nguiConfig) {
             return {
                 restrict: 'A',
-                replace: true,
+                require: '^nguiGridview',
+                transclude: true,
+                //replace: true,
                 scope: {
-                    gridview: '=nguiGridview'
+                    field: '@nguiGridviewSorting'
                 },
                 templateUrl: function (elem, attrs) {
-                    return attrs.templateUrl || $nguiConfig.baseTemplateUrl + '/gridview.htm';
+                    return attrs.templateUrl || $nguiConfig.baseTemplateUrl + '/gridview/title.htm';
                 },
-                controller: ['$scope',
-                    function ($scope) {
-                        var $gridview = $scope.$gridview = $scope.gridview || $nguiGridview();
-
-                        this.addColumn = function (column) {
-                            $gridview.addColumn(column);
-                        };
+                link: function ($scope, $elem, $attrs, $ctrl, $tran) {
+                    $scope.$soring = {
+                        get status() {
+                            return $ctrl.$gridview.sorting[$scope.field];
+                        },
+                        sort: function (flag) {
+                            var old = $ctrl.$gridview.sorting[$scope.field];
+                            if (old !== flag) {
+                                $ctrl.$gridview.sorting[$scope.field] = flag;
+                                $ctrl.$gridview.$onChange();
+                            }
+                        }
                     }
-                ]
+                    $elem.append($tran());
+                }
+            };
+        }
+    ]);
+
+    nguiModule.directive('nguiGridviewFilterNumber', ['$nguiConfig',
+        function ($nguiConfig) {
+            return {
+                restrict: 'A',
+                require: '^nguiGridview',
+                scope: {
+                    field: '@nguiGridviewFilterNumber'
+                },
+                templateUrl: function (elem, attrs) {
+                    return attrs.templateUrl || $nguiConfig.baseTemplateUrl + '/gridview/filter-number.htm';
+                },
+                link: function ($scope, $elem, $attrs, $ctrl, $tran) {
+                    $scope.$filter = {
+                        get query() {
+                            return $ctrl.$gridview.filter[$scope.field];
+                        },
+                        set query(value) {
+                            var old = $ctrl.$gridview.filter[$scope.field];
+                            if (old !== value) {
+                                $ctrl.$gridview.filter[$scope.field] = value;
+                                $ctrl.$gridview.$onChange();
+                            }
+                        }
+                    };
+                }
+            };
+        }
+    ]);
+
+    nguiModule.directive('nguiGridviewFilterText', ['$nguiConfig',
+        function ($nguiConfig) {
+            return {
+                restrict: 'A',
+                require: '^nguiGridview',
+                scope: {
+                    field: '@nguiGridviewFilterText'
+                },
+                templateUrl: function (elem, attrs) {
+                    return attrs.templateUrl || $nguiConfig.baseTemplateUrl + '/gridview/filter-text.htm';
+                },
+                link: function ($scope, $elem, $attrs, $ctrl, $tran) {
+                    $scope.$filter = {
+                        get query() {
+                            return $ctrl.$gridview.filter[$scope.field];
+                        },
+                        set query(value) {
+                            var old = $ctrl.$gridview.filter[$scope.field];
+                            if (old !== value) {
+                                $ctrl.$gridview.filter[$scope.field] = value;
+                                $ctrl.$gridview.$onChange();
+                            }
+                        }
+                    };
+                }
             };
         }
     ]);
