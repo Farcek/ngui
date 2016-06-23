650449eec6a3b3791790fd8fdc020cfc54f3791a
Farcek <farcek@gmail.com>
2016-06-23 11:41:28 +0900

    fix paging

--- paging/paging.js
+++ paging/paging.js
@@ -6,74 +6,9 @@
     /* global angular */
     var ui = angular.module('ngui-paging', []);
     ui
-        .factory('$pagingFactory', [
-            function () {
-                return function (options) {
-                    options = options || {};
-                    var o = {
-                        total: options.total || 0,
-                        page: options.page || 1,
-                        limit: options.limit || 10,
-                        maxPage: options.maxPage || 10,
 
-                        print: function () {
-                            return o.total > 0;
-                        },
-                        totalPage: function () {
-                            return Math.ceil(o.total / o.limit);
-                        },
-                        startPage: function () {
-                            var pr = Math.round(o.maxPage / 2);
-                            var p = o.page - pr;
-                            return p < 1 ? 1 : p;
-                        },
-                        endPage: function () {
-                            var pr = Math.round(o.maxPage / 2), t = o.totalPage();
-                            var p = o.page + pr;
-                            return p > t ? t : p;
-                        },
-                        showPages: function () {
-                            var r = [];
-                            for (var i = o.startPage(); i <= o.endPage(); i++) {
-                                r.push(i);
-                            }
-                            return r;
-                        },
-                        hasFirst: function () {
-                            return o.startPage() > 1;
-                        },
-                        hasFirstRange: function () {
-                            return o.startPage() > 2;
-                        },
-                        hasLast: function () {
-                            return o.endPage() < o.totalPage();
-                        },
-                        hasLastRange: function () {
-                            return o.endPage() < o.totalPage() - 1;
-                        },
-                        update: function (data) {
-                            if (data) {
-                                o.total = data.total;
-                                o.page = data.page || o.page;
-                                o.limit = data.limit || o.limit;
-                                o.maxPage = data.maxPage || o.maxPage;
-                            }
-                        },
-                        queryString: function () {
-                            return JSON.stringify({
-                                page: o.page,
-                                limit: o.limit
-                            });
-                        }
-
-                    };
-
-                    return o;
-                };
-            }
-        ])
-        .directive('nguiPaging', ['$nguiConfig', '$pagingFactory',
-            function ($nguiConfig, $pagingFactory) {
+        .directive('nguiPaging', ['$nguiConfig',
+            function ($nguiConfig) {
                 return {
                     templateUrl: function (elem, attrs) {
                         return attrs.templateUrl || $nguiConfig.baseTemplateUrl + '/paging/paging.htm';
@@ -95,10 +30,6 @@
                         // var child = scope.$new();
                         // child.test = "kk";
                         // el.append(tran( scope ));
-                        scope.cp = function (p) {
-                            scope.active = p;
-
-                        };
 
 
                         var $paging = scope.$paging = {
@@ -118,11 +49,15 @@
                                 return scope.maxPage || 10;
                             },
                             get active() {
-                                return scope.active || 1;
+                                if (scope.active <= $paging.totalPage && scope.active > 0) {
+                                    return scope.active;
+                                }
+
+                                return 1;
                             },
                             get totalPage() {
                                 var limit = $paging.limit;
-                                return limit && Math.ceil($paging.total / limit);
+                                return (limit && Math.ceil($paging.total / limit)) || 0;
                             },
                             get startPage() {
                                 var pr = Math.round($paging.maxPage / 2);
@@ -151,7 +86,7 @@
                                 return $paging.endPage < $paging.totalPage;
                             },
                             get hasLastRange() {
-                                return $paging.endPage < $paging.totalPage;
+                                return $paging.endPage < $paging.totalPage - 1;
                             }
                         };
                     }
